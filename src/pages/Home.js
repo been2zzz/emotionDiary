d407ec469b72cf3cf29969c3e6601df0c5292ade
devGo20 <devbin20@gmail.com>
2022-06-10 14:56:34 +0900

    [CREATE] diary date filter

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,36 @@
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { DiaryStateContext } from '../App';
 
 import MyHeader from './../components/MyHeader';
 import MyButton from './../components/MyButton';
+import DiaryList from './../components/DiaryList';
 
 const Home = () => {
+  const diaryList = useContext(DiaryStateContext);
+
+  const [data, setData] = useState([]);
   const [currentDate, setCurrentDate] = useState(new Date());
   const headText = `${currentDate.getFullYear()}.${currentDate.getMonth() + 1}`;
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const firstDay = new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth(),
+        1
+      ).getTime();
+
+      const lastDay = new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth() + 1,
+        0
+      ).getTime();
+
+      setData(
+        diaryList.filter((it) => firstDay <= it.date && it.date <= lastDay)
+      );
+    }
+  }, [diaryList, currentDate]);
 
   const increaseMonth = () => {
     setCurrentDate(
@@ -32,8 +57,13 @@ const Home = () => {
         leftChild={<MyButton text={'<'} onClick={decreaseMonth} />}
         rightChild={<MyButton text={'>'} onClick={increaseMonth} />}
       />
+      <DiaryList diaryList={data} />
     </div>
   );
+};
+
+DiaryList.defaultProps = {
+  diaryList: [],
 };
 
 export default Home;
