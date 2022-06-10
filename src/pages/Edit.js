7fe3f346bd4321766939366518450d495f364b82
devGo20 <devbin20@gmail.com>
2022-06-10 10:28:07 +0900

    [study] react-rotuer-dom custom hooks

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,7 +1,18 @@
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
 const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams(); // searchParams 상태 변화 함수도 제공
+  const id = searchParams.get('id');
+  const mode = searchParams.get('mode');
   return (
     <div>
       <h1>Edit</h1>
+      <button onClick={() => setSearchParams({ who: 'GO' })}>
+        Query String Change!
+      </button>
+      <button onClick={() => navigate('/home')}>HOME으로 가기</button>
+      <button onClick={() => navigate(-1)}>뒤로 가기</button>
     </div>
   );
 };
