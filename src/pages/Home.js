913388763a268fd557a6ed6b54063a98cb46cc59
devGo20 <devbin20@gmail.com>
2022-06-10 14:34:01 +0900

    [CREATE] Home Header, 전후 월 이동

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,37 @@
+import { useState } from 'react';
+
+import MyHeader from './../components/MyHeader';
+import MyButton from './../components/MyButton';
+
 const Home = () => {
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const headText = `${currentDate.getFullYear()}.${currentDate.getMonth() + 1}`;
+
+  const increaseMonth = () => {
+    setCurrentDate(
+      new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth() + 1,
+        currentDate.getDate()
+      )
+    );
+  };
+  const decreaseMonth = () => {
+    setCurrentDate(
+      new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth() - 1,
+        currentDate.getDate()
+      )
+    );
+  };
   return (
     <div>
-      <h1>HOME</h1>
+      <MyHeader
+        headText={headText}
+        leftChild={<MyButton text={'<'} onClick={decreaseMonth} />}
+        rightChild={<MyButton text={'>'} onClick={increaseMonth} />}
+      />
     </div>
   );
 };
